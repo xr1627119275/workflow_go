6a7c169431d6556cc0cba4f701067f0a84f70d4c
xrdev <1627119275@qq.com>
2020-11-12 11:25:49 +0800

    change file

--- hello.go
+++ hello.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 )
+
+func runCommand(command string) io.Writer {
+	cmdName := "cmd.exe"
+	cmdArgs := []string{"/c", command}
+	fmt.Println("Running command: " + command)
+	cmd := exec.Command(cmdName, cmdArgs...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	cmd.Run()
+	return cmd.Stdout
+
+}
 
 func main() {
 	fmt.Println(os.Args)
 	fmt.Println(os.Getenv("development"))
+
+	fmt.Println(runCommand("dir C:"))
+	fmt.Println(runCommand("dir D:"))
+	fmt.Println(runCommand("dir E:"))
+	fmt.Println("结束")
 }
